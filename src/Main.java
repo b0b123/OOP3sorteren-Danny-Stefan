2a6ab0bbce8f2fe68e9f0afdd3ab2bb176d0873f
b0b123 <s.voogd@st.hanze.nl>
2017-03-07 14:09:43 +0100

    start GUI

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,42 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import net.sentientturtle.OOP3Sorteren.BubbleStepSort;
 
 import java.util.Arrays;
 import java.util.Random;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage){
+        Barchart pane = new Barchart();
+        pane.setStyle("-fx-border-color: black");
+
+        Button step = new Button("Step");
+
+        HBox hBox = new HBox(5);
+        hBox.getChildren().addAll(step);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(pane);
+        borderPane.setBottom(hBox);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(borderPane, 600, 250);
+        primaryStage.setTitle("sorteren"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+    }
     public static void main(String[] args) {
+        launch(args);
+
+
         Random random = new Random();
         Integer[] data = new Integer[20];
         for (int i = 0; i < data.length; i++) {
@@ -15,4 +47,8 @@ public class Main {
             System.out.println(Arrays.toString(bubbleStepSort.getData()) + "\t" + bubbleStepSort.index);
         } while (!bubbleStepSort.step());
     }
+
+}
+class Barchart extends Pane{
+
 }
